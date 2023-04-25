73a4986eec2ba166bc5403c0122342b4e5145158
abellopmar <92862957+abellopmar@users.noreply.github.com>
2023-04-25 20:23:43 +0200

    palabras esp y mitan ingles

--- diccionarioTRA.pl
+++ diccionarioTRA.pl
@@ -8,6 +8,45 @@ det(las,the).
 det(un, a).
 det(una, a).
 det(mi, my).
+det(la, the).
+det(este, this).
+det(el, the).
+det(los, the).
+det(aquella, that).
+det(las, the).
+det(un, a).
+det(esas, those).
+det(una, a).
+det(su, his).
+det(sus, their).
+det(este, this).
+det(nuestro, our).
+det(solo, just).
+det(esta, this).
+det(nuestra, our).
+det(donde, where).
+det(tu, your).
+det(ese, that).
+det(otro, another).
+det(unas, some).
+det(esa, that).
+det(eso, that).
+det(mis, my).
+det(algunas, some).
+det(menos, less).
+det(esos, those).
+det(alguna, some).
+det(estos, these).
+det(unos, some).
+det(aquel, that).
+det(esto, this).
+det(aquellos, that).
+det(otras, other).
+det(nuestros, our).
+det(algunos, some).
+det(algun, any).
+det(estas, these).
+det(muchas, a lot of).
 
 nombre(esp, n(X)) --> [X], {n(X,_)}.
 nombre(eng, n(X)) --> [Y], {n(X,Y)}.
@@ -42,6 +81,276 @@ n(vecino, neighbor).
 n(escribir, writing).
 n(rocodromo, climbing-wall).
 n(tardes, afternoon).
+n(veces, times).
+n(embargo, nevertheless).
+n(partido, match).
+n(pesetas, coins).
+n(personas, people).
+n(orden, order).
+n(grupo, group).
+n(cuenta, account).
+n(misma, same).
+n(programa, program).
+n(palabras, words).
+n(segunda, second).
+n(empresa, business).
+n(puesto, position).
+n(cosas, things).
+n(ciudad, city).
+n(lo, it).
+n(libro, book).
+n(manera, manner).
+n(politico, politician).
+n(persona, person).
+n(sistema, system).
+n(ellas, they).
+n(le, him).
+n(historia, history).
+n(total, total).
+n(me, i).
+n(juan, john).
+n(tipo, type).
+n(dios, god).
+n(cuatro, four).
+n(condiciones, conditions).
+n(punto, point).
+n(mejico, mexico).
+n(fuerza, force).
+n(ello, it).
+n(todo, everything).
+n(noche, night).
+n(accion, action).
+n(ser, being).
+n(amor, love).
+n(agua, water).
+n(policia, police).
+n(dos, two).
+n(puerta, door).
+n(haber, there is).
+n(pesar, to weight).
+n(situacion, situation).
+n(zona, zone).
+n(calle, street).
+n(anos, years).
+n(ejemplo, example).
+n(musica, music).
+n(acuerdo, agreement).
+n(vista, view).
+n(usted, you).
+n(campo, field).
+n(estados, states).
+n(que, what).
+n(nadie, nobody).
+n(saber, knowledge).
+n(paises, countries).
+n(obras, works).
+n(yo, i).
+n(horas, hours).
+n(razon, reason).
+n(ex, ex).
+n(vez, time).
+n(tarde, late).
+n(ninos, kids).
+n(ley, law).
+n(presencia, presence).
+n(todos, everyone).
+n(tema, topic).
+n(guerra, war).
+n(dinero, money).
+n(nos, we).
+n(desarrollo, development).
+n(comision, commission).
+n(proceso, process).
+n(antonio, anthony).
+n(parte, part).
+n(realidad, reality).
+n(servicio, service).
+n(sentido, way).
+n(hijo, son).
+n(el, he).
+n(lado, side).
+n(ultima, last).
+n(uno, one).
+n(ciento, hundread).
+n(cambio, change).
+n(hablar, to speak).
+n(tiempo, time).
+n(mismo, same).
+n(mano, hand).
+n(minutos, minutes).
+n(produccion, production).
+n(estar, to be).
+n(camino, path).
+n(san, saint).
+n(seis, six).
+n(numero, number).
+n(quien, whom).
+n(vida, life).
+n(sociedad, society).
+n(fondo, background).
+n(direccion, direction).
+n(centro, center).
+n(papel, paper).
+n(te, tea).
+n(padre, father).
+n(otros, other).
+n(gente, people).
+n(barcelona, barcelona).
+n(final, final).
+n(idea, idea).
+n(relacion, relationship).
+n(cuerpo, body).
+n(obra, work).
+n(dado, dice).
+n(otra, another).
+n(base, base).
+n(gobierno, government).
+n(capital, capital).
+n(ultimo, last).
+n(ambos, both).
+n(madre, mother).
+n(europa, europe).
+n(libertad, freedom).
+n(dia, day).
+n(modo, mode).
+n(relaciones, relationships).
+n(problema, problem).
+n(espacio, space).
+n(ella, she).
+n(cinco, five).
+n(medios, media).
+n(tres, three).
+n(carlos, charles).
+n(ir, to go).
+n(hombres, men).
+n(informacion, information).
+n(poblacion, population).
+n(ojos, eyes).
+n(empresas, businesses).
+n(muerte, death).
+n(estudio, study).
+n(pais, country).
+n(nombre, name).
+n(salud, health).
+n(servicios, services).
+n(haya, beech).
+n(ano, year).
+n(mujeres, women).
+n(principio, principle).
+n(siglo, century).
+n(estado, state).
+n(cultura, culture).
+n(meses, months).
+n(manana, tomorrow).
+n(forma, shape).
+n(media, half).
+n(caso, case).
+n(nosotros, we).
+n(nada, nothing).
+n(hora, hour).
+n(primeros, firsts).
+n(hacer, to do).
+n(arte, art).
+n(general, general).
+n(pueblo, town).
+n(paz, peace).
+n(sector, sector).
+n(dar, to give).
+n(imagen, image).
+n(problema, problem).
+n(medida, measure).
+n(presidente, president).
+n(don, mr).
+n(mayor, elder).
+n(datos, data).
+n(tu, you).
+n(consejo, advice).
+n(les, they).
+n(verdad, truth).
+n(interes, interest).
+n(algo, something).
+n(julio, july).
+n(grupos, groups).
+n(casa, house).
+n(miembros, members).
+n(ellos, they).
+n(ninguna, none).
+n(cabeza, head).
+n(cara, face).
+n(primera, first).
+n(edad, age).
+n(luis, louise).
+n(movimiento, movement).
+n(tierra, ground).
+n(equipo, team).
+n(momento, moment).
+n(segundo, second).
+n(puntos, points).
+n(millones, millions).
+n(director, director).
+n(actividad, activity).
+n(espana, spain).
+n(uso, to use).
+n(casos, cases).
+n(nino, kid).
+n(manos, hands).
+n(nivel, level).
+n(hoy, today).
+n(futuro, future).
+n(lugar, place).
+n(familia, family).
+n(madrid, madrid).
+n(mes, month).
+n(partir, split).
+n(trabajo, work).
+n(falta, lack).
+n(llegar, to arrive).
+n(ministro, minister).
+n(decir, to say).
+n(cosa, thing).
+n(primero, first).
+n(posibilidad, posibility).
+n(seguridad, security).
+n(dias, days).
+n(resultados, results).
+n(educacion, education).
+n(politica, politics).
+n(atencion, atention).
+n(gonzalez, gonzalez).
+n(respecto, regard).
+n(capacidad, capacity).
+n(semana, week).
+n(efecto, efect).
+n(varios, several).
+n(real, real).
+n(valor, value).
+n(pasado, past).
+n(aire, air).
+n(primer, first).
+n(voz, voice).
+n(investigacion, investigation).
+n(medio, half).
+n(paso, step).
+n(siguiente, next).
+n(senor, sir).
+n(figura, figure).
+n(mil, thousand).
+n(central, central).
+n(comunidad, comunity).
+n(proyecto, proyect).
+n(necesidad, need).
+n(mercado, market).
+n(serie, series).
+n(poder, power).
+n(mayoria, mayority).
+n(organizacion, organization).
+n(luz, light).
+n(calidad, quality).
+n(frente, forehead).
+n(fin, end).
+n(propia, own).
+n(tener, to own).
+n(sobre, envelope).
 
 verbo(esp, v(X)) --> [X], {v(X,_)}.
 verbo(eng, v(X)) --> [Y], {v(X,Y)}.
@@ -63,6 +372,66 @@ v(sirve, serves).
 v(cazo, hunted).
 v(vimos, saw).
 v(era, was).
+v(era).
+v(ver).
+v(iba).
+v(pueden).
+v(quiere).
+v(se).
+v(tienen).
+v(fueron).
+v(van).
+v(es).
+v(he).
+v(como).
+v(sera).
+v(ha).
+v(creo).
+v(tengo).
+v(dice).
+v(son).
+v(parece).
+v(fue).
+v(habia).
+v(sabe).
+v(era).
+v(esta).
+v(habian).
+v(hizo).
+v(hubiera).
+v(han).
+v(hay).
+v(puede).
+v(tiene).
+v(estoy).
+v(dio).
+v(eran).
+v(hace).
+v(dijo).
+v(sido).
+v(siendo).
+v(estaba).
+v(deben).
+v(da).
+v(podria).
+v(seria).
+v(existe).
+v(hecho).
+v(llego).
+v(estan).
+v(podia).
+v(soy).
+v(hacia).
+v(estaban).
+v(sigue).
+v(hemos).
+v(debe).
+v(trata).
+v(tuvo).
+v(se).
+v(va).
+v(sea).
+v(tenia).
 
 adjetivo(esp, adj(X)) --> [X], {adj(X,_)}.
 adjetivo(eng, adj(X)) --> [Y], {adj(X,Y)}.
@@ -79,6 +448,45 @@ adj(potente, powerful).
 adj(lento, slow).
 adj(agil, agile).
 adj(delicado, delicate).
+adj(espanol).
+adj(buena).
+adj(nueva).
+adj(internacional).
+adj(propia).
+adj(social).
+adj(igual).
+adj(ultimos).
+adj(espanola).
+adj(unico).
+adj(bajo).
+adj(grandes).
+adj(buen).
+adj(posible).
+adj(importante).
+adj(bien).
+adj(especial).
+adj(diferentes).
+adj(actual).
+adj(gran).
+adj(publico).
+adj(alto).
+adj(personal).
+adj(unidos).
+adj(visto).
+adj(dicho).
+adj(bueno).
+adj(cierto).
+adj(dificil).
+adj(joven).
+adj(largo).
+adj(nacional).
+adj(pronto).
+adj(nuevos).
+adj(nuevo).
+adj(cerca).
+adj(mal).
+adj(necesario).
+adj(nuevas).
 
 conjuncion(esp, conj(X)) --> [X], {conj(X,_)}.
 conjuncion(eng, conj(X)) --> [Y], {conj(X,Y)}.
@@ -87,6 +495,9 @@ conj(e, and).
 conj(ni, neither).
 conj(pero, but).
 conj(aunque, although).
+conj(o).
+conj(sino).
+conj(mientras).
 
 adverbio(esp, adv(X)) --> [X], {adv(X,_)}.
 adverbio(eng, adv(X)) --> [Y], {adv(X,Y)}.
@@ -98,6 +509,63 @@ adv(muy, very).
 adv(bastante, quite).
 adv(ayer, yesterday).
 adv(solamente, only).
+adv(cual).
+adv(no).
+adv(ahi).
+adv(fin).
+adv(mas).
+adv(muchos).
+adv(si).
+adv(dentro).
+adv(ya).
+adv(cuando).
+adv(cualquier).
+adv(aun).
+adv(tambien).
+adv(fuera).
+adv(muy).
+adv(interior).
+adv(tampoco).
+adv(ningun).
+adv(porque).
+adv(solo).
+adv(asi).
+adv(alli).
+adv(ahora).
+adv(cada).
+adv(despues).
+adv(demas).
+adv(incluso).
+adv(traves).
+adv(tan).
+adv(durante).
+adv(siempre).
+adv(tanto).
+adv(si).
+adv(antes).
+adv(todavia).
+adv(anterior).
+adv(poco).
+adv(ayer).
+adv(junto).
+adv(mucho).
+adv(etc.).
+adv(cuanto).
+adv(ademas).
+adv(quien).
+adv(pues).
+adv(mejor).
+adv(propio).
+adv(entonces).
+adv(todas).
+adv(como).
+adv(casi).
+adv(toda).
+adv(tal).
+adv(luego).
+adv(aqui).
+adv(quienes).
+adv(nunca).
 
 preposicion(esp, prep(X)) --> [X], {prep(X,_)}.
 preposicion(eng, prep(X)) --> [Y], {prep(X,Y)}.
@@ -106,3 +574,16 @@ prep(de, of).
 prep(para, to).
 prep(en, in).
 prep(por, by).
+prep(con).
+prep(tras).
+prep(al).
+prep(sin).
+prep(sobre).
+prep(entre).
+prep(hasta).
+prep(desde).
+prep(segun).
+prep(contra).
+prep(mediante).
+prep(ante).
+prep(hacia).
