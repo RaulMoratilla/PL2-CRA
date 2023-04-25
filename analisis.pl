709503c37c69d5c3a218e911c579e285e820e0a0
RaulMoratilla <79266992+RaulMoratilla@users.noreply.github.com>
2023-04-25 15:43:23 +0200

    Comp funciona

--- analisis.pl
+++ analisis.pl
@@ -94,12 +94,12 @@ conj(e).
 conj(ni).
 conj(pero).
 conj(aunque).
+conj(mientras).
 
 adverbio(adv(X)) --> [X], {adv(X)}.
 adv(que).
 adv(cuando).
 adv(donde).
-adv(mientras).
 adv(muy).
 adv(bastante).
 adv(ayer).
@@ -112,10 +112,16 @@ prep(para).
 prep(en).
 prep(por).
 
+aplanar_comp(comp(X), Y) :- aplanar_comp(X, Y).
+aplanar_comp(X, X) :- \+ compound(X).
+aplanar_comp(X, Y) :- compound(X), functor(X, F, N), functor(Y, F, N), aplanar_comp_args(N, X, Y).
+aplanar_comp_args(N, X, Y) :- N > 0, arg(N, X, Xn), aplanar_comp(Xn, Yn), arg(N, Y, Yn), N1 is N-1, aplanar_comp_args(N1, X, Y).
+aplanar_comp_args(0, _, _).
+
 % Reglas gramaticales
 
-oracion(X, O, Y) :- compuesta(X, O, Y).
-oracion(X, O, Y) :- simple(X, O, Y).
+oracion(X, O, Y) :- compuesta(X1, O, Y), aplanar_comp(X1, X).
+oracion(X, O, Y) :- simple(X1, O, Y), aplanar_comp(X1, X).
 
 compuesta(ocm(OCM)) --> coordinada(OCM).
 
@@ -127,20 +133,20 @@ coordinada(oc(O,CONJ,O2)) --> simple(O), conjuncion(CONJ), compuesta(O2).
 
 subordinada(or(ADV, O)) --> adverbio(ADV), oracion(O).
 
-complementos(GADJ) --> g_adjetival(GADJ).
-complementos(GADV) --> g_adverbial(GADV).
-complementos(GPREP) --> g_preposicional(GPREP).
-complementos(GN) --> g_nominal(GN).
-complementos(SUB) --> subordinada(SUB).
-complementos(comp(SUB, COMP)) --> subordinada(SUB), complementos(COMP).
+complementos(comp(GADJ)) --> g_adjetival(GADJ).
+complementos(comp(GADV)) --> g_adverbial(GADV).
+complementos(comp(GPREP)) --> g_preposicional(GPREP).
+complementos(comp(GN)) --> g_nominal(GN).
+complementos(comp(SUB)) --> subordinada(SUB).
 complementos(comp(CONJ, GN)) --> conjuncion(CONJ), g_nominal(GN).
+complementos(comp(SUB, COMP)) --> subordinada(SUB), complementos(COMP).
 complementos(comp(GADJ, COMP)) --> g_adjetival(GADJ), complementos(COMP).
 complementos(comp(GADV, COMP)) --> g_adverbial(GADV), complementos(COMP).
 complementos(comp(GPREP, COMP)) --> g_preposicional(GPREP), complementos(COMP).
 complementos(comp(GN, COMP)) --> g_nominal(GN), complementos(COMP).
 complementos(comp(CONJ, GN, COMP)) --> conjuncion(CONJ), g_nominal(GN), complementos(COMP).
 
-g_verbal(gv(V,COMP)) --> verbo(V), complementos(COMP).
+g_verbal(gv(V, COMP)) --> verbo(V), complementos(COMP).
 g_verbal(gv(V)) --> verbo(V).
 
 g_nominal(gn(N, COMP)) --> nombre(N), complementos(COMP).
@@ -163,17 +169,25 @@ g_preposicional(gp(P,GN)) --> preposicion(P), g_nominal(GN).
 g_preposicional(gp(P,GADJ)) --> preposicion(P), g_adjetival(GADJ).
 g_preposicional(gp(P,GADV)) --> preposicion(P), g_adverbial(GADV).
 
-o_prueba([jose,es,moreno,y,maria,es,alta]).
-o_prueba([jose,estudia,filosofia,pero,maria,estudia,derecho]).
-o_prueba([maria,toma,un,cafe,mientras,jose,recoge,la,mesa]).
-o_prueba([jose,toma,cafe,y,lee,el,periodico]).
-o_prueba([jose,y,hector,comen,patatas,fritas,y,beben,cerveza]).
-o_prueba([jose,come,patatas,fritas,pero,maria,prefiere,paella,aunque,hector,toma,cafe,e,irene,lee,una,novela]).
-o_prueba([irene,canta,y,salta,mientras,jose,estudia]).
-o_prueba([hector,come,patatas,fritas,y,bebe,zumo,mientras,jose,canta,y,salta,aunque,maria,lee,una,novela]).
-o_prueba([jose,que,es,agil,escala,en,el,rocodromo,por,las,tardes]).
-o_prueba([jose,que,es,muy,delicado,come,solamente,manzanas,rojas]).
-o_prueba([el,procesador,de,textos,que,es,una,herramienta,bastante,potente,sirve,para,escribir,documentos]).
-o_prueba([el,procesador,de,textos,es,una,herramienta,muy,potente,que,sirve,para,escribir,documentos,pero,es,bastante,lento]).
-o_prueba([el,raton,que,cazo,el,gato,era,gris]).
-o_prueba([el,hombre,que,vimos,ayer,era,mi,vecino]).
+o_prueba(1, [jose,es,moreno,y,maria,es,alta]).
+o_prueba(2, [jose,estudia,filosofia,pero,maria,estudia,derecho]).
+o_prueba(3, [maria,toma,un,cafe,mientras,jose,recoge,la,mesa]).
+o_prueba(4, [jose,toma,cafe,y,lee,el,periodico]).
+o_prueba(5, [jose,y,hector,comen,patatas,fritas,y,beben,cerveza]).
+o_prueba(6, [jose,come,patatas,fritas,pero,maria,prefiere,paella,aunque,hector,toma,cafe,e,irene,lee,una,novela]).
+o_prueba(7, [irene,canta,y,salta,mientras,jose,estudia]).
+o_prueba(8, [hector,come,patatas,fritas,y,bebe,zumo,mientras,jose,canta,y,salta,aunque,maria,lee,una,novela]).
+o_prueba(9, [jose,que,es,agil,escala,en,el,rocodromo,por,las,tardes]).
+o_prueba(10, [jose,que,es,muy,delicado,come,solamente,manzanas,rojas]).
+o_prueba(11, [el,procesador,de,textos,que,es,una,herramienta,bastante,potente,sirve,para,escribir,documentos]).
+o_prueba(12, [el,procesador,de,textos,es,una,herramienta,muy,potente,que,sirve,para,escribir,documentos,pero,es,bastante,lento]).
+o_prueba(13, [el,raton,que,cazo,el,gato,era,gris]).
+o_prueba(14, [el,hombre,que,vimos,ayer,era,mi,vecino]).
+
+ejecutar_pruebas(INI, INI).
+ejecutar_pruebas(INI, FIN) :- 
+    o_prueba(INI, O),
+    oracion(X, O, []),
+    draw(X),
+    INI2 is INI+1,
+    ejecutar_pruebas(INI2, FIN).
