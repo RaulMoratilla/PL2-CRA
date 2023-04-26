a855cbfe833fc84ab75a9a7fa500372890f532fb
RaulMoratilla <79266992+RaulMoratilla@users.noreply.github.com>
2023-04-26 22:28:04 +0200

    Estructurado

--- procesamiento.pl
+++ procesamiento.pl
@@ -1,6 +1,9 @@
-:-consult("draw.pl").
-:-consult("diccionarioESP.pl").
+% APLANAR
 
+/**
+ * Aplana cada uno de los N argumentos del compound
+ * de X (todo lo que tenga comp(...)) y los mete en Y
+ */
 aplanar_args(_, _, 0).
 
 aplanar_args(X, Y, N) :-
@@ -21,6 +24,10 @@ aplanar_args(X, Y, N) :-
 
 aplanar_args(X, X, _).
 
+/**
+ * Aplana el compound X (todo lo que tenga comp(...))
+ * y lo devuelve en Y
+ */
 aplanar_comp(X, Y) :-
     aplanar_iter(X, Y1, NACT, NAPL),
     NACT =\= NAPL,
@@ -33,12 +40,19 @@ aplanar_comp(X, Y) :-
     functor(Y, F, N),
     aplanar_args(Y1, Y, N).
 
+/**
+ * Hace una iteracion de aplanar el compound X (todo lo que tenga comp(...))
+ * y lo devuelve en Y
+ */
 aplanar_iter(X, Y, NACT, NAPL) :-
     functor(X, F, NACT),
     args_aplanado(X, NACT, NAPL),
     functor(Y, F, NAPL),
     add_args(X, Y, 1, 1, NACT).
 
+/**
+ * Devuelve el numero de argumentos que tendra el compound aplanado
+ */
 args_aplanado(_, 0, 0).
 
 args_aplanado(X, NACT, NAPL) :-
@@ -70,6 +84,10 @@ args_aplanado(X, NACT, NAPL) :-
 args_aplanado(_, X, X) :- 
     X > 0.
 
+/**
+ * Anade a Y todos los compounds de X que no sean comp(...)
+ * Los comp(...) los aplana y los anade a Y
+ */
 add_args(_, _, IX, _, NFIN) :- IX > NFIN.
 
 add_args(X, Y, IX, IY, NFIN) :-
@@ -90,6 +108,12 @@ add_args(X, Y, IX, IY, NFIN) :-
     IYN is IY + 1,
     add_args(X, Y, IXN, IYN, NFIN).
 
+% AJUSTAR COMPUESTAS
+
+/**
+ * Devuelve True si encuentra una oracion subordinada en el arbol X
+ * Accede al arbol de forma recursiva
+ */
 buscar_subordinada(X, N, _) :-
     arg(N, X, ARG),
     compound(ARG),
@@ -106,6 +130,10 @@ buscar_subordinada(X, N, N1) :-
     (N2 is N + 1,
     buscar_subordinada(X, N2, N1))).
 
+/**
+ * Ayuda a iterar el arbol sintactico buscando si hay, y en 
+ * su caso donde se encuentra una oracion subordinada
+ */
 ajustar_compuestas_args(_, _, N, M) :- N > M.
 
 ajustar_compuestas_args(X, Y, N, M) :-
@@ -125,6 +153,10 @@ ajustar_compuestas_args(X, Y, N, M) :-
     N1 is N + 1,
     ajustar_compuestas_args(X, Y, N1, M).
 
+/**
+ * Busca oraciones subordinadas dentro de oraciones simples,
+ * y en caso de encontrarlas transforma las simples en compuestas
+ */
 ajustar_compuestas(X, X) :-
     \+ compound(X).
 
@@ -143,89 +175,100 @@ ajustar_compuestas(X, Y) :-
     functor(Y, F, N),
     ajustar_compuestas_args(X, Y, 1, N).
 
+% SEPARAR ORACIONES EN COMPOUNDS
 
-% Reglas Gramaticales
+/**
+ * Separa las oraciones que se encuentren en X en compounds. Es posible que se 
+ * encuentren guiones (que no significan nada) por la forma en la que estan
+ * construidos los compounds en prolog y la imposibilidad de crear uno vacio
+ */
+separar(X, oraciones(-)) :-
+    \+ compound(X).
 
-oracion(X, O, Y) :- compuesta(X1, O, Y), once(aplanar_comp(X1, X2)), ajustar_compuestas(X2, X).
-oracion(X, O, Y) :- simple(X1, O, Y), once(aplanar_comp(X1, X2)), ajustar_compuestas(X2, X).
+separar(X, C) :-
+    compound(X),
+    functor(X, ocm, M),
+    buscar_subordinada(X, 1, M),
+    separar_args(X, C1, M),
+    quitar_subordinada(X, X1, 1, M),
+    functor(C1, F, N),
+    N1 is N+1,
+    functor(C, F, N1),
+    copy_compound(C1, C, 1, 2, M),
+    arg(1, C, X1).
 
-compuesta(ocm(OCM)) --> coordinada(OCM).
+separar(X, C) :-
+    compound(X),
+    functor(X, F, N),
+    F \= os,
+    separar_args(X, C, N).
 
-simple(os(GN, GV)) --> g_nominal(GN), g_verbal(GV).
-simple(os(GV)) --> g_verbal(GV).
+separar(X, C) :-
+    compound(X),
+    functor(X, os, M),
+    separar_args(X, C1, M),
+    functor(C1, F, N),
+    N1 is N+1,
+    functor(C, F, N1),
+    copy_compound(C1, C, 1, 2, M),
+    arg(1, C, X).
 
-coordinada(oc(O,CONJ,O2)) --> simple(O), conjuncion(CONJ), simple(O2).
-coordinada(oc(O,CONJ,O2)) --> simple(O), conjuncion(CONJ), compuesta(O2).
+/**
+ * Elimina las oraciones subordinadas de X
+ * obteniendo en X1 el resto de la oracion
+ */
+quitar_subordinada(X, X1, N, M) :-
+    N =< M,
+    functor(X, F, M),
+    arg(N, X, A),
+    compound(A),
+    functor(A, or, _),
+    TAM is M-1,
+    functor(X1, F, TAM),
+    copy_compound(X, X1, 1, 1, N-1),
+    copy_compound(X, X1, N+1, N, M).
 
-subordinada(or(ADV, O)) --> adverbio(ADV), oracion(O).
+quitar_subordinada(X, X1, N, M) :-
+    N =< M,
+    functor(X, F, M),
+    arg(N, X, A),
+    compound(A),
+    functor(A, F1, TAM),
+    F1 \= os,
+    N1 is N+1,
+    ((quitar_subordinada(A, A1, 1, TAM),
+    functor(X1, F, M),
+    arg(N, X1, A1),
+    copy_compound(X, X1, N1, N1, M),
+    N2 is N-1,
+    copy_compound(X, X1, 1, 1, N2));
+    quitar_subordinada(X, X1, N1, M)).
 
-complementos(comp(SUB)) --> subordinada(SUB).
-complementos(comp(GADJ)) --> g_adjetival(GADJ).
-complementos(comp(GADV)) --> g_adverbial(GADV).
-complementos(comp(GPREP)) --> g_preposicional(GPREP).
-complementos(comp(GN)) --> g_nominal(GN).
-complementos(comp(CONJ, GN)) --> conjuncion(CONJ), g_nominal(GN).
-complementos(comp(SUB, COMP)) --> subordinada(SUB), complementos(COMP).
-complementos(comp(GADJ, COMP)) --> g_adjetival(GADJ), complementos(COMP).
-complementos(comp(GADV, COMP)) --> g_adverbial(GADV), complementos(COMP).
-complementos(comp(GPREP, COMP)) --> g_preposicional(GPREP), complementos(COMP).
-complementos(comp(GN, COMP)) --> g_nominal(GN), complementos(COMP).
-complementos(comp(CONJ, GN, COMP)) --> conjuncion(CONJ), g_nominal(GN), complementos(COMP).
+/**
+ * Ayuda a iterar el arbol sintactico iterando los nodos hermanos
+ * y los nodos hijos
+ */
+separar_args(_, oraciones(-), 0).
 
-g_verbal(gv(V, COMP)) --> verbo(V), complementos(COMP).
-g_verbal(gv(V)) --> verbo(V).
+separar_args(X, C, N) :-
+    arg(N, X, A),
+    \+ compound(A),
+    N1 is N-1,
+    separar_args(X, C2, N1),
+    concatenar_compound(oraciones(-), C2, C).
 
-g_nominal(gn(N, COMP)) --> nombre(N), complementos(COMP).
-g_nominal(gn(N)) --> nombre(N).
-g_nominal(gn(DET, N, COMP)) --> determinante(DET), nombre(N), complementos(COMP).
-g_nominal(gn(DET, N)) --> determinante(DET), nombre(N).
+separar_args(X, C, N) :-
+    arg(N, X, A),
+    separar(A, C1),
+    N1 is N-1,
+    separar_args(X, C2, N1),
+    concatenar_compound(C1, C2, C).
 
-g_adjetival(gadj(A)) --> adjetivo(A).
-g_adjetival(gadj(GADJ,GADJ2)) --> adjetivo(GADJ), g_adjetival(GADJ2).
-g_adjetival(gadj(GADJ,GN)) --> adjetivo(GADJ), g_nominal(GN).
-g_adjetival(gadj(GADJ,GP)) --> adjetivo(GADJ), g_preposicional(GP).
-g_adjetival(gadj(ADV,GADJ)) --> adverbio(ADV), g_adjetival(GADJ).
-g_adjetival(gadj(GADJ,CONJ,GADJ2)) --> adjetivo(GADJ), conjuncion(CONJ), g_adjetival(GADJ2).
+% SUJETO
 
-g_adverbial(gadv(ADV)) --> adverbio(ADV).
-g_adverbial(gadv(GADV,GADV2)) --> adverbio(GADV), g_adverbial(GADV2).
-g_adverbial(gadv(GADV,GN)) --> adverbio(GADV), g_preposicional(GN).
-
-g_preposicional(gp(P,GN)) --> preposicion(P), g_nominal(GN).
-g_preposicional(gp(P,GADJ)) --> preposicion(P), g_adjetival(GADJ).
-g_preposicional(gp(P,GADV)) --> preposicion(P), g_adverbial(GADV).
-
-o_prueba(1, [jose,es,moreno,y,maria,es,alta]).
-o_prueba(2, [jose,estudia,filosofia,pero,maria,estudia,derecho]).
-o_prueba(3, [maria,toma,un,cafe,mientras,jose,recoge,la,mesa]).
-o_prueba(4, [jose,toma,cafe,y,lee,el,periodico]).
-o_prueba(5, [jose,y,hector,comen,patatas,fritas,y,beben,cerveza]).
-o_prueba(6, [jose,come,patatas,fritas,pero,maria,prefiere,paella,aunque,hector,toma,cafe,e,irene,lee,una,novela]).
-o_prueba(7, [irene,canta,y,salta,mientras,jose,estudia]).
-o_prueba(8, [hector,come,patatas,fritas,y,bebe,zumo,mientras,jose,canta,y,salta,aunque,maria,lee,una,novela]).
-o_prueba(9, [jose,que,es,agil,escala,en,el,rocodromo,por,las,tardes]).
-o_prueba(10, [jose,que,es,muy,delicado,come,solamente,manzanas,rojas]).
-o_prueba(11, [el,procesador,de,textos,que,es,una,herramienta,bastante,potente,sirve,para,escribir,documentos]).
-o_prueba(12, [el,procesador,de,textos,es,una,herramienta,muy,potente,que,sirve,para,escribir,documentos,pero,es,bastante,lento]).
-o_prueba(13, [el,raton,que,cazo,el,gato,era,gris]).
-o_prueba(14, [el,hombre,que,vimos,ayer,era,mi,vecino]).
-
-ejecutar_pruebas(INI, INI).
-ejecutar_pruebas(INI, FIN) :- 
-    o_prueba(INI, O),
-    oracion(X, O, []),
-    draw(X),
-    separar(X, C),
-    formatear_oraciones(C, C1),
-    write(C1), nl,
-    INI2 is INI+1,
-    ejecutar_pruebas(INI2, FIN).
-
-formatear_oraciones(X, L) :-
-    functor(X, _, N),
-    get_oraciones(X, L1, N),
-    poner_sujeto(L1, L, []).
-
+/**
+ * Busca el sujeto de una oracion. Si lo encuentra devuelve true
+ */
 buscar_sujeto(O, O) :-
     functor(O, gn, _).
 
@@ -235,10 +278,17 @@ buscar_sujeto(O, SUJ) :-
     arg(1, O, A),
     buscar_sujeto(A, SUJ).
 
+/**
+ * Llama a buscar sujeto para que se pueda iterar de forma recursiva
+ */
 get_sujeto(O, SUJ) :-
     oracion(X, O, []),
     buscar_sujeto(X, SUJ).
 
+/**
+ * En caso de que la oracion tenga sujeto, se anade al inicio de la oracion
+ * siempre que no este anadido ya
+ */
 poner_sujeto([], [], _).
 
 poner_sujeto([O|RO], [O|ROS], _) :-
@@ -250,6 +300,19 @@ poner_sujeto([O|RO], [OS|ROS], SUJ) :-
     append(SUJ, O, OS),
     poner_sujeto(RO, ROS, SUJ).
 
+% DAR FORMATO DE LISTA A LAS ORACIONES Y PONER EL SUJETO
+
+/**
+ * Formatea las oraciones para que se puedan imprimir de forma correcta (en listas) y ponerles el sujeto
+ */
+formatear_oraciones(X, L) :-
+    functor(X, _, N),
+    get_oraciones(X, L1, N),
+    poner_sujeto(L1, L, []).
+
+/**
+ * Obtiene del compound oraciones(...) cada una de las oraciones en listas
+ */
 get_oraciones(_, [], 0).
 
 get_oraciones(X, L, OACT) :-
@@ -291,82 +354,8 @@ get_oracion(X, L, N) :-
     N2 is N-1,
     get_oracion(X, L2, N2),
     append(L2, [A], L).
-    
 
-separar(X, oraciones(-)) :-
-    \+ compound(X).
-
-separar(X, C) :-
-    compound(X),
-    functor(X, ocm, M),
-    buscar_subordinada(X, 1, M),
-    separar_args(X, C1, M),
-    quitar_subordinada(X, X1, 1, M),
-    functor(C1, F, N),
-    N1 is N+1,
-    functor(C, F, N1),
-    copy_compound(C1, C, 1, 2, M),
-    arg(1, C, X1).
-
-separar(X, C) :-
-    compound(X),
-    functor(X, F, N),
-    F \= os,
-    separar_args(X, C, N).
-
-separar(X, C) :-
-    compound(X),
-    functor(X, os, M),
-    separar_args(X, C1, M),
-    functor(C1, F, N),
-    N1 is N+1,
-    functor(C, F, N1),
-    copy_compound(C1, C, 1, 2, M),
-    arg(1, C, X).
-
-quitar_subordinada(X, X1, N, M) :-
-    N =< M,
-    functor(X, F, M),
-    arg(N, X, A),
-    compound(A),
-    functor(A, or, _),
-    TAM is M-1,
-    functor(X1, F, TAM),
-    copy_compound(X, X1, 1, 1, N-1),
-    copy_compound(X, X1, N+1, N, M).
-
-quitar_subordinada(X, X1, N, M) :-
-    N =< M,
-    functor(X, F, M),
-    arg(N, X, A),
-    compound(A),
-    functor(A, F1, TAM),
-    F1 \= os,
-    N1 is N+1,
-    ((quitar_subordinada(A, A1, 1, TAM),
-    functor(X1, F, M),
-    arg(N, X1, A1),
-    copy_compound(X, X1, N1, N1, M),
-    N2 is N-1,
-    copy_compound(X, X1, 1, 1, N2));
-    quitar_subordinada(X, X1, N1, M)).
-
-separar_args(_, oraciones(-), 0).
-
-separar_args(X, C, N) :-
-    arg(N, X, A),
-    \+ compound(A),
-    N1 is N-1,
-    separar_args(X, C2, N1),
-    concatenar_compound(oraciones(-), C2, C).
-
-separar_args(X, C, N) :-
-    arg(N, X, A),
-    separar(A, C1),
-    N1 is N-1,
-    separar_args(X, C2, N1),
-    concatenar_compound(C1, C2, C).
-
+% AUXILIARES
 
 copy_compound(_, _, ACTX, _, MAX) :- ACTX > MAX.
 
